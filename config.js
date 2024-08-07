7dcdd37d465e6e7f01391d0944f1ee8c88c1e366
Ansariwrites <160799296+Ansariwrites@users.noreply.github.com>
2024-08-07 19:27:44 +0500

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ if (!ownervb){
 
 const ownerlist = ownervb.split(';');
 
-global.owner = [];
+global.owner = [923288666219], 'Mr Doramon', ;
 for (let i = 0; i < ownerlist.length; i += 2) {
     const owner = [
         ownerlist[i],            
